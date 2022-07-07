72ac7103b6c16f01634ccc1e5e6642e5bb7befc6
skurtz97 <seankurtz75@gmail.com>
2022-07-07 03:19:41 -0500

    connection and skeleton

--- main.go
+++ main.go
@@ -3,15 +3,42 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"os"
 
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-func main() {
+type Handler struct {
+	db *gorm.DB
+}
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+func NewHandler(db *gorm.DB) http.Handler {
+	h := &Handler{db: db}
+	r := mux.NewRouter()
+	r.HandleFunc("/", h.handleRoot).Methods("GET")
+	r.HandleFunc("/user/{id}", h.handleUser).Methods("GET")
+	return r
+}
+
+func main() {
+	// Load environment variables from file.
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("failed to load environment variables: %v", err)
 	}
+
+	// Connect to PlanetScale database using DSN environment variable.
+	db, err := gorm.Open(mysql.Open(os.Getenv("DSN")), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
+	if err != nil {
+		log.Fatalf("failed to connect to planetscale: %v", err)
+	}
+
+	// Create an API handler which serves data from PlanetScale.
+	handler := NewHandler(db)
 	fmt.Println("first commit")
 }
